276e0e6e520ecf45fb7aa1e8b5d0b017e1c8d667
Thom Mondeaux <Thommond554@gmail.com>
2024-09-03 15:40:58 -0400

    Set up a basic Read me file.

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,9 @@
 public class VendingMachine {
     public static void main(String[] args) {
-        
+        Boolean exit = false;
+
+        while (exit != true) {
+            exit = true;
+        }
     }
 }
